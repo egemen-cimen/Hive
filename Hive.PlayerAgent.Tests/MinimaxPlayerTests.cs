ddb1529020fa916735bd56adb08b93c9f178f0e2
Egemen Çimen <ecimen96@gmail.com>
2026-07-28 23:58:26 +0200

    Added another test

--- Hive.PlayerAgent.Tests/MinimaxPlayerTests.cs
+++ Hive.PlayerAgent.Tests/MinimaxPlayerTests.cs
@@ -6,7 +6,43 @@ namespace Hive.PlayerAgent.Tests
     [TestClass]
     public class MinimaxPlayerTests
     {
-        // TODO: use game rules to generate game states. also write a test helper for this
+        [TestMethod]
+        public void Given_GameStateWhereWhiteCanWinInOneMove_When_SuggestedNextMoveRetrieved_Then_ReturnsWinningMove()
+        {
+            // GIVEN
+            var minimaxPlayer = new MinimaxPlayer();
+            var gameState = GameRules.CreateFreshGameState();
+            // Spawn first pieces in a line
+            ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(SpiderPiece) && a.DestinationCoordinate.column == 0);
+            ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(SpiderPiece) && a.DestinationCoordinate.column == 0);
+            // Spawn queens as second pieces in a line and get the coordinates
+            ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(QueenPiece) && a.DestinationCoordinate.column == 0);
+            var queenSpawnAction = ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(QueenPiece) && a.DestinationCoordinate.column == 0);
+            var spacesNextToQueen = gameState.CoordinateSystem.GetAdjacentCoordinates(queenSpawnAction.DestinationCoordinate);
+            // Don't surround white queen & also save ants for the finishing move
+            ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() != typeof(AntPiece) && a.DestinationCoordinate.column == 0);
+            // Finally spawn next 4 pieces near black queen to surround it
+            ApplyPlayerSpawnActionPredicateToGameState(gameState, a => spacesNextToQueen.Contains(a.DestinationCoordinate));
+            for (var i = 0; i < 3; i++)
+            {
+                // Don't surround white queen & also spawn the ants
+                ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(AntPiece) && a.DestinationCoordinate.column == 0);
+
+                // Continue surrounding the black queen
+                ApplyPlayerSpawnActionPredicateToGameState(gameState, a => spacesNextToQueen.Contains(a.DestinationCoordinate));
+            }
+
+            // WHEN
+            var suggestedAction = minimaxPlayer.SuggestNextPlayerAction(gameState);
+
+            // THEN
+            // TODO: check if the game state is not modified after the method call
+            Assert.IsInstanceOfType<PlayerMovementAction>(suggestedAction);
+            Assert.Contains(((PlayerMovementAction)suggestedAction).DestinationCoordinate, spacesNextToQueen);
+            GameRules.ApplyPlayerActionToGameState(gameState, suggestedAction);
+            Assert.IsTrue(GameRules.VerifyWhetherGameStateIsTerminal(gameState));
+            Assert.AreEqual(GameResult.WHITE_WON, GameRules.GetGameResult(gameState));
+        }
 
         [TestMethod]
         public void Given_GameStateWhereBlackCanWinInOneMove_When_SuggestedNextMoveRetrieved_Then_ReturnsWinningMove()
@@ -21,7 +57,7 @@ namespace Hive.PlayerAgent.Tests
             var queenSpawnAction = ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(QueenPiece) && a.DestinationCoordinate.column == 0);
             var spacesNextToQueen = gameState.CoordinateSystem.GetAdjacentCoordinates(queenSpawnAction.DestinationCoordinate);
             ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() == typeof(QueenPiece) && a.DestinationCoordinate.column == 0);
-            // Finally spawn next 5 pieces near white queen to surround it (9 pieces in total)
+            // Spawn next 4 pieces near white queen to surround it (9 pieces in total)
             ApplyPlayerSpawnActionPredicateToGameState(gameState, a => spacesNextToQueen.Contains(a.DestinationCoordinate));
             // Don't surround black queen & also save ants for the finishing move
             ApplyPlayerSpawnActionPredicateToGameState(gameState, a => a.PieceToSpawn.GetType() != typeof(AntPiece) && a.DestinationCoordinate.column == 0);
@@ -41,9 +77,12 @@ namespace Hive.PlayerAgent.Tests
             var suggestedAction = minimaxPlayer.SuggestNextPlayerAction(gameState);
 
             // THEN
+            // TODO: check if the game state is not modified after the method call
             Assert.IsInstanceOfType<PlayerMovementAction>(suggestedAction);
             Assert.Contains(((PlayerMovementAction)suggestedAction).DestinationCoordinate, spacesNextToQueen);
             GameRules.ApplyPlayerActionToGameState(gameState, suggestedAction);
+            Assert.IsTrue(GameRules.VerifyWhetherGameStateIsTerminal(gameState));
+            Assert.AreEqual(GameResult.BLACK_WON, GameRules.GetGameResult(gameState));
         }
 
 
