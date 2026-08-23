650b0bbc99f89cd95c01f77ff67d64431a1fbf30
Egemen Çimen <ecimen96@gmail.com>
2026-08-23 12:04:47 +0200

    Added preprocessor directives for DebuggerDisplay

--- Hive.Core/Models/Hexagon.cs
+++ Hive.Core/Models/Hexagon.cs
@@ -1,8 +1,12 @@
-﻿using System.Diagnostics;
+﻿#if DEBUG
+using System.Diagnostics;
+#endif
 
 namespace Hive.Core.Models
 {
+#if DEBUG
     [DebuggerDisplay("Hexagon with {GetPieceCount()} piece(s): {PeekPiece().Color} {PeekPiece().GetPieceName()}")]
+#endif
     public class Hexagon
     {
         private Stack<IPiece> Pieces { get; } = new Stack<IPiece>();
